06404beac19083477cdbf408703787fd81b8dd4d
steve <stevedanko1@gmail.com>
2014-02-12 13:57:24 -0500

    fiz buzz solution

---
+++ fizz_buzz.rb
@@ -0,0 +1,21 @@
+def fizz_buzz(starting_number, ending_number)
+  (starting_number..ending_number).each do |n|
+    if n.to_i % 3 == 0
+      if n % 3 == 0 && n % 5 == 0
+        puts "FizzBuzz"
+      else
+        puts "Fizz"
+      end
+    elsif n.to_i % 5 == 0
+      if n % 3 == 0 && n % 5 == 0
+        puts "FizzBuzz"
+      else
+        puts "Buzz"
+      end
+    else 
+      puts n
+    end
+  end
+end
+
+fizz_buzz(1,30)
