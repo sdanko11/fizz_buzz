748954e652d11cc9040d31f0bfb2bae26a697372
steve <stevedanko1@gmail.com>
2014-02-12 14:03:58 -0500

    removed to_i

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,12 +1,12 @@
 def fizz_buzz(starting_number, ending_number)
   (starting_number..ending_number).each do |n|
-    if n.to_i % 3 == 0
+    if n % 3 == 0
       if n % 3 == 0 && n % 5 == 0
         puts "FizzBuzz"
       else
         puts "Fizz"
       end
-    elsif n.to_i % 5 == 0
+    elsif n % 5 == 0
       if n % 3 == 0 && n % 5 == 0
         puts "FizzBuzz"
       else
